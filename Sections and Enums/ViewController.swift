efcafc3c134a763120465f827952c6ba33bea82e
Iker-hub <ikergargu@gmail.com>
2022-02-15 12:11:29 +0100

    Initial Commit

---
+++ Sections and Enums/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sections and Enums
+//
+//  Created by DAM09 on 15/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
